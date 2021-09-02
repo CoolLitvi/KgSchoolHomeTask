63b7894584769366736e9518a356919c402cd2d8
Volodymyr Litvinchuk <volodymyr.litvinchuk@kindgeek.com>
2021-09-03 02:19:45 +0300

    complete task1

--- home-task-1/task1.js
+++ home-task-1/task1.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+function drawTriangle(height) {
+    let lineToPrint = '';
+
+    for(let i = 0; i < height; i++){
+        lineToPrint = lineToPrint + ' #';
+        console.log(lineToPrint);
+    }
+}
+
+drawTriangle(3);
+
